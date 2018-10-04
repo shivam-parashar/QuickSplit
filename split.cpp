65c805f7ffcde768986e40c66d62f5bcbc555cf6
Shivam Parashar <mailtoageek@gmail.com>
2018-10-04 20:39:54 +0530

    Adding extra transactions functionality

--- split.cpp
+++ split.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main()
 {
-    int i, n, m, u, v, w, mini, x, maxi, y;
+    int i, n, m, u, v, w, mini, x, maxi, y,up;
     cout<<"Enter the number of people and the number of transaction"<<endl;
     cin >> n >> m;
-    int debt[n+1];
-    for(i=0; i<=n; i++) 
-    {
-        debt[i]=0;
-    }
+    int debt[n+100];
+
+    memset(debt,0,sizeof(debt));
+
     cout<<"ThankYou!"<<endl;
     cout<<"In the next "<<m<<" lines " <<"please enter the index of the person who paid the amount followed by the index of the person who owe it"<<endl;
 
@@ -22,45 +21,77 @@ int main()
         debt[v] = debt[v] + w;
     }
 
-    multiset< pair<int, int > > S;
-
-    for(i=0; i<n; i++)
+    int t1=1,t2=0,t3=0;
+    while(t1==1)
     {
-        if(debt[i] != 0) S.insert(make_pair(debt[i], i));
-    } 
-           
-    int TotalTransactions = 0;
-
-    while(!S.empty()) 
-    {
-        pair<int, int > p1 = *S.begin(); 
-        S.erase(S.begin());
-        mini = p1.first;
-        x = p1.second;
-
-        pair<int, int > p2 = *S.rbegin(); 
-        S.erase(prev(S.end()));
-        maxi = p2.first;
-        y = p2.second;
-
-        int amount = min(-mini, maxi);
-        
-        TotalTransactions++; 
-        // mini pays amount "amount" to maxi
-        cout<<x<<" pays amount "<<amount<<" to "<<y<<endl;
-        mini = mini + amount;
-        maxi = maxi - amount;
-        
-        if(mini)
+        if(t3==1)
         {
-            S.insert(make_pair(mini, x));
+            cout<<"Enter the number of transactions you want to add "<<endl;
+            cin>>up; 
+            cout<<"In the next "<<up<<" lines " <<"please enter the index of the person who paid the amount followed by the index of the person who owe it"<<endl;
+            for(i=0; i<up; i++) 
+            {
+                //u has to pay v an amount w
+                cin >> u >> v >> w;
+                debt[u] = debt[u] - w;
+                debt[v] = debt[v] + w;
+            }
         }
-        if(maxi) 
+
+        multiset< pair<int, int > > S;
+
+        for(i=0; i<n; i++)
         {
-            S.insert(make_pair(maxi, y));
+            if(debt[i] != 0) S.insert(make_pair(debt[i], i));
+        } 
+               
+        int TotalTransactions = 0;
+
+        while(!S.empty()) 
+        {
+            pair<int, int > p1 = *S.begin(); 
+            S.erase(S.begin());
+            mini = p1.first;
+            x = p1.second;
+
+            pair<int, int > p2 = *S.rbegin(); 
+            S.erase(prev(S.end()));
+            maxi = p2.first;
+            y = p2.second;
+
+            int amount = min(-mini, maxi);
+            
+            TotalTransactions++; 
+            // mini pays amount "amount" to maxi
+            cout<<x<<" pays amount "<<amount<<" to "<<y<<endl;
+            mini = mini + amount;
+            maxi = maxi - amount;
+            
+            if(mini)
+            {
+                S.insert(make_pair(mini, x));
+            }
+            if(maxi) 
+            {
+                S.insert(make_pair(maxi, y));
+            }
+        }
+        
+        t1=0;
+        
+        cout<<"Do you want to add more transaction ?"<<endl;
+        cout<<"if yes then press 1 else press 0"<<endl;
+        cin>>t3;
+
+        if(t3==1)
+        {
+            t1=1;
+        }
+        else  if(t3==0)
+        {
+            break;
         }
     }
-    
     cout <<"Total number of Transactions occurred are "<<TotalTransactions << endl;
     return 0;
 }
